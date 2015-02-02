5a7ceed78a021b07eef9449112a060ab4ec2899a
armadilloNik <nik@tumpedOver.com>
2015-02-01 23:10:22 -0600

    added basic HO examples

--- src/CSharpSigExamples/Example1/Program.cs
+++ src/CSharpSigExamples/Example1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,41 @@ namespace Example1
     {
         static void Main(string[] args)
         {
+            var asParameter = HigherOrderFunctionAsParameter(10, x => x*x);
+            
+            Console.WriteLine(asParameter);
+            Console.ReadLine();
+
+            var asReturnValue = HigherOrderFunctionAsReturnValue();
+
+            var result = asReturnValue(1, 5);
+            
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            var both = HigherOrderFunctionBoth(x => x*x);
+
+            var bothResult = both(1, 5);
+            Console.WriteLine(bothResult);
+            Console.ReadLine();
+
+
         }
+
+        static int HigherOrderFunctionAsParameter(int myVerySpecialArgument, Func<int, int> myFunction)
+        {
+            return myFunction(myVerySpecialArgument);
+        }
+
+        static Func<int, int, int> HigherOrderFunctionAsReturnValue()
+        {
+            return (x, y) => x + y;
+        }
+
+        static Func<int, int, int> HigherOrderFunctionBoth(Func<int, int> myFunction)
+        {
+            return (x, y) => myFunction(x) + myFunction(y);
+        }
+
     }
 }
