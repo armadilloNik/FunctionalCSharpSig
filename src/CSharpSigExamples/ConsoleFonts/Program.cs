32907cec7df0718ce79f04010facc1d939413517
armadilloNik <nik@tumpedOver.com>
2015-02-04 11:41:02 -0600

    organizing examples

---
+++ src/CSharpSigExamples/ConsoleFonts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFonts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.ForegroundColor = ConsoleColor.Black;
+            
+            Console.WriteLine("\thello");
+            Console.WriteLine("\thello");
+            Console.WriteLine("\thello");
+            Console.WriteLine("\thello");
+            Console.WriteLine("\thello");
+            Console.WriteLine("\thello");
+
+            Console.ReadLine();
+
+            //var fonts = ConsoleHelper.ConsoleFonts;
+            //for (int f = 0; f < fonts.Length; f++)
+            //    Console.WriteLine("{0}: X={1}, Y={2}",
+            //       fonts[f].Index, fonts[f].SizeX, fonts[f].SizeY);
+            //ConsoleHelper.SetConsoleFont(5);
+            //ConsoleHelper.SetConsoleIcon(SystemIcons.Information);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ src/CSharpSigExamples/Exmaple_03_Construction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exmaple_03_Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
