a26bccdf1eebe19e34862a27c2e5950f0b8a37f0
armadilloNik <nik@tumpedOver.com>
2015-02-04 21:55:55 -0600

    side effect example

--- src/CSharpSigExamples/Example_02_Purity/Program.cs
+++ src/CSharpSigExamples/Example_02_Purity/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example_02_Purity
@@ -10,6 +11,28 @@ namespace Example_02_Purity
     {
         static void Main(string[] args)
         {
+            //hidden side effect
+
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(" {0}", SideEffectSum(2, 3));
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int SideEffectSum(int a, int b)
+        {
+            return a + b + ReadValueFromDb();
+        }
+
+        //ok so not really database
+        private static int ReadValueFromDb()
+        {
+            Thread.Sleep(1000);
+            return new Random().Next() % 2;
         }
     }
 }
